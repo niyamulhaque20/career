749d6eb81323d9a33ebe74b1a712c7300bd1a5ea
Niyamul Haque <niyamulhaque78@gmail.com>
2026-06-23 19:34:16 +0600

    Fix build issues

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBSl2wed9ibYVPC_u78JRxrvcaT6jU-uC0",
   authDomain: "careeros-3d334.firebaseapp.com",
@@ -13,12 +13,22 @@ const firebaseConfig = {
   storageBucket: "careeros-3d334.firebasestorage.app",
   messagingSenderId: "110278323056",
   appId: "1:110278323056:web:f726fbd346ace2b06e7db5",
-  measurementId: "G-XPMVTTSN5V"
+  measurementId: "G-XPMVTTSN5V",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Analytics
+let analytics = null;
+
+try {
+  analytics = getAnalytics(app);
+} catch (error) {
+  console.log("Analytics not available");
+}
+
+// Firebase services
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
